b5bc035f4e1ff203430369e04587ce84e89a89fa
unknown <Natto>
2024-12-07 10:47:19 +0800

    update six-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -361,16 +361,11 @@ LIMIT 1
 -- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
 
-SELECT
-    "CREDIT_PACKAGE".name AS 組合包名稱,
-    COUNT(*) AS  銷售數量
-FROM "CREDIT_PURCHASE"
-JOIN "CREDIT_PACKAGE" ON "CREDIT_PACKAGE".id = "CREDIT_PURCHASE".credit_package_id
-WHERE "CREDIT_PURCHASE".created_at >= '2024-11-01 00:00:00' and "CREDIT_PURCHASE".created_at <= '2024-11-30 23:59:59'
-GROUP BY "CREDIT_PACKAGE".name;
+
 
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
+
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
